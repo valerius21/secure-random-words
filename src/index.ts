bfd61b3cbb67c9223769218396041dbcdae5a230
Valerius Mattfeld <mail@valerius.me>
2021-12-09 15:58:17 +0100

    add: default wordlist

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
-import { words } from './words';
+import { words as defaultWords } from './words';
 import { randomSync } from 'pure-random-number';
 
 /**
  * Generates and array of random words. Maximal length of the words is defined by the `maxLength` parameter.
  * @param numberOfWords Number of unique words to generate
  */
-export const randomWords = async (numberOfWords = 1): Promise<string[]> => {
+export const randomWords = async (
+  numberOfWords = 1,
+  words = defaultWords
+): Promise<string[]> => {
   if (numberOfWords > words.length) {
     throw new Error(
       'Number of words requested is greater than the number of words available'
@@ -22,6 +25,6 @@ export const randomWords = async (numberOfWords = 1): Promise<string[]> => {
   return await Promise.all(Array.from(result));
 };
 
-export const maxLength = words.length;
+export const maxLength = defaultWords.length;
 
-export const wordList = words;
+export const wordList = defaultWords;
